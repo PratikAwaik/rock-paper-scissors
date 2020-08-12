af3acb0b50a23ae7dce3bfd1ded103a912c137ed
PratikAwaik <pratikawaik125@gmail.com>
2020-08-12 13:55:25 +0530

    Added a simple UI and RPS images

--- script.js
+++ script.js
@@ -1,77 +1,135 @@
 // Random choice for computer
 function computerPlay() {
-    let choice = ["rock", "paper", "scissors"]
-    return choice[Math.floor(Math.random() * choice.length)]
+    const choices = document.getElementsByClassName("computer-image");
+    return choices[Math.floor(Math.random() * choices.length)];
 }
 
-// Decides who wins the match
-function playRound(playerSelection, computerSelection) {
+// Decides who wins the match and scores accordingly
+// [playerscore, computerscore]
+function playRound(player, computer) {
     let result;
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
     if (player == "rock") {
         if (computer == "rock") {
-            result = ["It's a tie!", 0, 0];
+            result = [0, 0];
         }
         else if (computer == "paper") {
-            result = ["You lose! Paper beats Rock!", 0, 1];
+            result = [0, 1];
         }
         else {
-            result = ["You win! Rock beats Scissors!", 1, 0];
+            result = [1, 0];
         }
     }
 
     else if (player == "paper") {
         if (computer == "rock") {
-            result = ["You win! Paper beats Rock!", 1, 0];
+            result = [1, 0];
         }
         else if (computer == "paper") {
-            result = ["It's a tie!", 0, 0];
+            result = [0, 0];
         }
         else{
-            result = ["You lose! Scissors beats Paper!", 0, 1];
+            result = [0, 1];
         }
     }
 
     else {
         if (computer == "rock") {
-            result = ["You lose! Rock beats Scissors!", 0, 1];
+            result = [0, 1];
         }
         else if (computer == "paper") {
-            result = ["You win! Scissors beats Paper!", 1, 0];
+            result = [1, 0];
         }
         else{
-            result = ["It's a tie!", 0, 0];
+            result = [0, 0];
         }
     }
 
     return result;
 }
 
+let playerScore = document.getElementById("player-score");
+let computerScore = document.getElementById("computer-score");
+let rounds = document.getElementById("rounds");
+let ties = document.getElementById("ties");
+
+playerScore.innerHTML = computerScore.innerHTML = rounds.innerHTML = ties.innerHTML = 0;
+
 // plays the game
 function game(elem) {
-    let playerChoice;
-    let playerScore = 0;
-    let computerScore = 0;
-    playerChoice = document.getElementById(elem.id).value;
-    document.getElementById("player-choice").innerHTML = playerChoice[0].toUpperCase() + playerChoice.slice(1);
-    let computerChoice = computerPlay();
-    document.getElementById("computer-choice").innerHTML = computerChoice[0].toUpperCase() + computerChoice.slice(1);
-    results = playRound(playerChoice, computerChoice); // results[0] will be used later.
-    playerScore += results[1];
-    computerScore += results[2];
+
+    let player = document.getElementById(elem.id);  // element
+    let computer = computerPlay();  // element 
+
+    computer.setAttribute("src", `images/${computer.id.slice(2)}red.png`);  // change the color for pc to red
+
+    let playerChoice = player.getAttribute("id"); 
+    let computerChoice = computer.getAttribute("id").slice(2);
+    
+    let roundResults = playRound(playerChoice, computerChoice);
+
+    if (roundResults[0] === 1) {
+        playerScore.innerHTML++;
+    }
+    else if (roundResults[1] === 1) {
+        computerScore.innerHTML++;
+    }
+    
+    rounds.innerHTML++
 
     let finalResult;
-    if (playerScore > computerScore) {
+    if (roundResults[0] > roundResults[1]) {
         finalResult = "Player wins";
     }
-    else if (computerScore > playerScore) {
+    else if (roundResults[1] > roundResults[0]) {
         finalResult = "Computer wins";
     }
     else {
         finalResult = "Game Tied";
+        ties.innerHTML++
+        
     }
-
+    
     document.getElementById("result").innerHTML = finalResult;
-    return;
+    return computer;
 }
+
+const images = document.querySelectorAll(".player-image");
+images.forEach(image => image.addEventListener("click", () => {
+        image.setAttribute("src", `images/${image.id}green.png`);
+        let pc = game(image);
+        let computer = pc.getAttribute("id").slice(2);
+
+        // change the image to default for player
+
+        if (image.id !== "rock" && image.id !== "paper") {
+            document.getElementById("rock").setAttribute("src", "images/rock.png");
+            document.getElementById("paper").setAttribute("src", "images/paper.png");
+        }
+
+        else if (image.id !== "paper" && image.id !== "scissors") {
+            document.getElementById("paper").setAttribute("src", "images/paper.png");
+            document.getElementById("scissors").setAttribute("src", "images/scissors.png");
+        }
+
+        else if (image.id !== "scissors" && image.id !== "rock") {
+            document.getElementById("rock").setAttribute("src", "images/rock.png");
+            document.getElementById("scissors").setAttribute("src", "images/scissors.png");
+        }
+
+        // change the image to default for computer
+
+        if (computer !== "rock" && computer !== "paper") {
+            document.getElementById("pcrock").setAttribute("src", "images/rock.png");
+            document.getElementById("pcpaper").setAttribute("src", "images/paper.png");
+        }
+
+        else if (computer !== "paper" && computer !== "scissors") {
+            document.getElementById("pcpaper").setAttribute("src", "images/paper.png");
+            document.getElementById("pcscissors").setAttribute("src", "images/scissors.png");
+        }
+
+        else if (computer !== "scissors" && computer !== "rock") {
+            document.getElementById("pcrock").setAttribute("src", "images/rock.png");
+            document.getElementById("pcscissors").setAttribute("src", "images/scissors.png");
+        } 
+}));
